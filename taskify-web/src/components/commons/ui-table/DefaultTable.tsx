7fbae07a449b460589dc7645feb8ea05893d6fc0
nakyoung98 <jay3925@cau.ac.kr>
2024-02-08 00:14:30 +0900

    Add: "DefaultTable 페이지네이션 조건부 렌더링 및 디자인 추가"

--- taskify-web/src/components/commons/ui-table/DefaultTable.tsx
+++ taskify-web/src/components/commons/ui-table/DefaultTable.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind';
 import CustomRowWithData, { DefaultRowProps } from './CustomRowWithData';
 import styles from './DefaultTable.module.scss';
 import { PaginationConfig, TableConfig } from './types';
+import PaginationButtonContainer from '../ui-pagination/PaginationButtonContainer';
 
 const cx = classNames.bind(styles);
 
@@ -20,6 +21,8 @@ export default function DefaultTable<P extends DefaultRowProps>({
   paginationConfig = {
     type: 'none',
     pageSize: 5,
+    leftClick: () => {},
+    rightClick: () => {},
   },
   columnNames,
   RowComponent,
@@ -28,6 +31,21 @@ export default function DefaultTable<P extends DefaultRowProps>({
 }: DefaultTableProps<P>) {
   return (
     <div className={cx('default-table')}>
+      {paginationConfig.type === 'pagination' ? (
+        <div className={cx('default-table__pagination-button-container')}>
+          <span
+            className={cx(
+              'default-table__pagination-button-container_page-info',
+            )}
+          >
+            * 페이지 중 *
+          </span>
+          <PaginationButtonContainer
+            leftClick={paginationConfig.leftClick!}
+            rightClick={paginationConfig.rightClick!}
+          />
+        </div>
+      ) : null}
       <h1 className={cx('default-table__title')}>{tableTitle}</h1>
       {children}
       <table className={cx('default-table__table')}>
