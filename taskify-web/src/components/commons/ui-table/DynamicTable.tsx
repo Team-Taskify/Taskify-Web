0bc3285b2d9434e58a6898a0d52e3ee4ee830ff7
nakyoung98 <jay3925@cau.ac.kr>
2024-02-08 12:26:02 +0900

    Fix: "DynamicTable 오류 수정"

--- taskify-web/src/components/commons/ui-table/DynamicTable.tsx
+++ taskify-web/src/components/commons/ui-table/DynamicTable.tsx
@@ -5,7 +5,6 @@ import DefaultTable, { DefaultTableProps } from './DefaultTable';
 export default function DynamicTable<P extends DefaultRowProps>(
   props: DefaultTableProps<P>,
 ) {
-    const 
   return (
     <PaginationConfigProvider>
       <DefaultTable {...props} />
